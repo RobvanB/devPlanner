23e50c4dd10500d0087bad06247affa7c4af2cf9
Rob vanBrandenburg <RobvanB@gmail.com>
2015-10-18 16:45:31 -0400

    Finish user edit, update, index and destroy actions

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -16,6 +16,21 @@ RSpec.describe User, type: :model do
     it { should respond_to(:remember_token) }
     it { should respond_to(:authenticate) }
 
+    it { should respond_to(:admin) }
+    it { should respond_to(:authenticate) }
+
+    it { should be_valid }
+    it { should_not be_admin }
+
+    describe "with admin attribute set to 'true'" do
+      before do
+        @user.save!
+        @user.toggle!(:admin)
+      end
+
+      it { should be_admin }
+    end
+
   end
 
   describe "remember token" do
--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -4,6 +4,53 @@ RSpec.describe "UserPages", type: :request do
 
   #subject { page }
 
+  describe "index" do
+
+    let(:user) { FactoryGirl.create(:user) }
+
+    before (:each) do
+      sign_in user
+      visit users_path
+    end
+
+    it { expect(page).to have_title('All users') }
+    it { expect(page).to have_selector('h1',text: 'All users') }
+
+    describe "pagination" do
+
+      before(:all) { 30.times { FactoryGirl.create(:user) } }
+      after(:all) { User.delete_all }
+
+      it { expect(page).to have_selector('div.pagination') }
+
+      it "should list each user" do
+        User.paginate(page: 1).each do |user|
+          expect(page).to have_selector('li', text: user.name)
+        end
+      end
+    end
+
+    describe "delete links" do
+
+      it { expect(page).to_not have_link('delete') }
+
+      describe "as an admin user" do
+        let(:admin) { FactoryGirl.create(:admin) }
+        before do
+          sign_in admin
+          visit users_path
+        end
+
+        it { expect(page).to have_link('delete', href: user_path(User.first)) }
+        it "should be able to delete another user" do
+          expect { click_link('delete') }.to change(User, :count).by(-1)
+        end
+        it { expect(page).to_not have_link('delete', href: user_path(admin)) }
+      end
+    end
+
+  end
+
   describe "signup page" do
     before { visit signup_path }
 
@@ -51,6 +98,44 @@ RSpec.describe "UserPages", type: :request do
         it { expect(page).to have_selector('div.alert.alert-success', text: 'Welcome') }
         it { expect(page).to have_link('Sign out') }
       end
+    end
+  end
+
+  describe "edit" do
+    let(:user) { FactoryGirl.create(:user) }
+    before do
+      sign_in user
+      visit edit_user_path(user)
+    end
+
+    describe "page" do
+      it { expect(page).to have_selector('h1',    text: "Update your profile") }
+      it { expect(page).to have_title("Edit user") }
+      it { expect(page).to have_link('change', href: 'http://gravatar.com/emails') }
+    end
+
+    describe "with invalid information" do
+      before { click_button "Save changes" }
+
+      it { expect(page).to have_content('error') }
+    end
+
+    describe "with valid information" do
+      let(:new_name)  { "New Name" }
+      let(:new_email) { "new@example.com" }
+      before do
+        fill_in "Name",             with: new_name
+        fill_in "Email",            with: new_email
+        fill_in "Password",         with: user.password
+        fill_in "Confirm Password", with: user.password
+        click_button "Save changes"
+      end
+
+      it { expect(page).to have_title(new_name) }
+      it { expect(page).to have_selector('div.alert.alert-success') }
+      it { expect(page).to have_link('Sign out', href: signout_path) }
+      specify { expect(user.reload.name).to  eq new_name }
+      specify { expect(user.reload.email).to eq new_email }
     end
   end
 end
